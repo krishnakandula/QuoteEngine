f43da6979c2ad283311829daed26bb5bfa6566e9
krishnakandula <krishna.c.kandula@gmail.com>
2017-04-30 13:34:59 -0500

    Add some documentation to MainContract

--- app/src/main/java/com/canvas/krishna/quoteengine/ui/MainContract.java
+++ app/src/main/java/com/canvas/krishna/quoteengine/ui/MainContract.java
@@ -12,13 +12,24 @@ public interface MainContract {
     interface View {
         void showLoading();
         void hideLoading();
-        void showQuotes(List<Quote> quotes, boolean reset);
+
+        /**
+         * Show quotes to user
+         * @param quotes List of quotes
+         * @param additionalQuotes whether or not to add new quotes to end of list
+         */
+        void showQuotes(List<Quote> quotes, boolean additionalQuotes);
         void showErrorMessage(String err);
     }
 
     interface Presenter {
         void setView(View view);
         void start();
+
+        /**
+         * Loads quotes from api
+         * @param additionalQuotes whether or not to add quotes to end of existing quotes
+         */
         void loadQuotes(boolean additionalQuotes);
         void onRefresh();
         void onLoadMoreQuotes();
